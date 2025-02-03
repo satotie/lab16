57b33e95807a242b6c5931b05669c5c53928f5d0
tie <tie.teiroom1234@gmail.com>
2025-02-03 12:59:26 +0700

    done

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *arr, int N, int M) {
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N * M; i++) {
+        cout << arr[i];
+        if((i + 1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+	for(int i = 0;i < N*M;i++){
+		a[i] = (rand()%101)*0.01;
+	}
+}
+
+void findRowSum(const double *arr, double *sum1, int N, int M) {
+    for(int i = 0; i < N; i++) {
+        sum1[i] = 0;
+        for(int j = 0; j < M; j++) {
+            sum1[i] += arr[i * M + j];
+        }
+    }
+}
+
+void findColSum(const double *arr, double *sum2, int N, int M) {
+    for(int j = 0; j < M; j++) {
+        sum2[j] = 0;
+        for(int i = 0; i < N; i++) {
+            sum2[j] += arr[i * M + j];
+        }
+    }
+}
